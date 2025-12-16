8e51fa018d6d3c5d4c7d435260f7bd41365d72d5
cku <xidanhgiaychuan@gmail.com>
2025-12-16 18:43:35 +0700

    bullet

--- space_shooter/shooter.js
+++ space_shooter/shooter.js
@@ -1,6 +1,6 @@
 let board;
-let boardHeight = 1000;
-let boardWidth = 1500;
+// let boardHeight = innerHeight;
+// let boardWidth = innerWidth;
 let context;
 let score = 0;
 let health = 100;
@@ -19,14 +19,14 @@ let bullet_speed = 10;
 
 // enemy
 
-let enemies = [];
+let enemiesArray = [];
 let enemyImg;
 let enemyWidth = 32;
 let enemyHeight = 32;
 
 // kits
 
-let healthKits = [];
+let healthKitsArray = [];
 let healthKitImg;
 let healthkitWidth = 32;
 let healthkitHeight = 32;
@@ -34,21 +34,21 @@ let healthkitHeight = 32;
 // === start==
 
 window.onload = function () {
+  // setup Board
   board = document.getElementById("board");
-  board.height = boardHeight;
-  board.width = boardWidth;
-
+  board.width = window.innerWidth;
+  board.height = window.innerHeight;
   context = board.getContext("2d");
 
   startGame();
 };
 
 function startGame() {
+  // xac dinh vi tri plyare
   mouse = {
     x: board.width / 2,
     y: board.height - 33,
   };
-
   touch = { x: innerWidth / 2, y: innerHeight - 33 };
 
   document.addEventListener("mousemove", function (event) {
@@ -77,13 +77,14 @@ function startGame() {
   healthKitImg.src = "./img/healthkit.png";
 
   // draw player
-
   player = new Player(playerImg, mouse.x, mouse.y, player_width, player_height);
-
-  update();
-
   // draw enemy
+  setInterval(drawEnemy, 3000);
+  setInterval(drawHelthKits, 5000);
+  update();
+}
 
+function drawEnemy() {
   for (let index = 0; index < 4; index++) {
     const x = Math.random() * (innerWidth - enemyWidth);
     const y = -enemyHeight;
@@ -93,8 +94,21 @@ function startGame() {
     const _enemy = new Player(enemyImg, x, y, width, height, speed);
 
     _enemy.draw();
+    enemiesArray.push(_enemy);
+  }
+}
 
-    enemies.push(_enemy);
+function drawHelthKits() {
+  for (let index = 0; index < 1; index++) {
+    const x = Math.random() * (innerWidth - enemyWidth);
+    const y = -enemyHeight;
+    const width = healthkitWidth;
+    const height = healthkitHeight;
+    const speed = Math.random() * 2.6;
+    const _healthyKit = new Player(healthKitImg, x, y, width, height, speed);
+
+    _healthyKit.draw();
+    healthKitsArray.push(_healthyKit);
   }
 }
 
@@ -102,10 +116,17 @@ function update() {
   requestAnimationFrame(update);
   context.clearRect(0, 0, board.width, board.height);
 
+  // cập nhật vị trí player theo chuột
+  player.x = mouse.x - player.width / 2; //để player nằm chính giữa chuột.
+  player.y = board.height - player.height - 10; // cố định ở dưới cùng
+
   player.draw();
 
-  enemies.forEach((enemy) => {
+  enemiesArray.forEach((enemy) => {
     enemy.update();
+  });
+  healthKitsArray.forEach((healthyKit) => {
+    healthyKit.update();
   });
 }
 
