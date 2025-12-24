d4066e4f424b05fd4c2b946c3f0f023729ab477c
cku <xidanhgiaychuan@gmail.com>
2025-12-24 11:11:46 +0700

    hay 15:52 rpg

---
+++ rpg_game/src/resoures.js
@@ -0,0 +1,32 @@
+class Resources {
+  constructor() {
+    // everything we plan to downlaod
+    this.toLoad = {
+      sky: "/sky.png",
+      ground: "/ground.png",
+      hero: "/hero-sheet.png",
+      shadow: "/shadow.png",
+    };
+    // a bucket to keep all of our images
+    this.images = {};
+
+    // load each image
+    Object.keys(this.toLoad).forEach((key) => {
+      const img = new Image();
+      img.src = this.toLoad[key];
+      //   add to images
+      this.images[key] = {
+        image: img,
+        isLoaded: false,
+      };
+
+      //   when imag loading successfuly
+      img.onload = () => {
+        this.images[key].isLoaded = true;
+      };
+    });
+  }
+}
+
+// create one instance for the whole app to use
+export const resources = new Resources();
